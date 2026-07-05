208e08c49fce5fecccc49ca65aefcee60709407a
Bilal <bilalasim3101@gmail.com>
2026-07-05 02:57:03 -0400

    Add CPU check

--- monitor.py
+++ monitor.py
@@ -1,8 +1,13 @@
 def check_memory():
     print("memory: OK")
 
+def check_cpu():
+    print("cpu: OK")
+
 def main():
     check_memory()
+    check_cpu()
+
 
 if __name__ == "__main__":
     main()
